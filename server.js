aa1669e59aabaaedd8aa7d40cebe3d8339d18627
Nirmal <rajnirmal1622@gmail.com>
2025-10-08 21:32:57 +0530

    update cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ connectDB();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173", // frontend URL
+    origin: "*", // frontend URL
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true, // allow cookies/authorization headers
   })
